315bdc9d4c35fc6b78af51d26c701904227e74ad
Jeff Bai <j.bai@wustl.edu>
2020-12-24 08:22:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,19 +2,22 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-          name: "Lord Montague"
+          name: "Lord Montague",
           age: "53"
         },
       :matriarch => {
-          name: "Lady Montague"
+          name: "Lady Montague",
           age: "54"
         },
       :hero => {
-          name: "Romeo"
-          age: "15"
+          name: "Romeo",
+          age: "15",
           status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Steven", age: "30", attitude: "confused"}
+      ]
    },
    :capulet => {
       :patriarch => {
