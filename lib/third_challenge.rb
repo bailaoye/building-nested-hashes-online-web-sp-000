04d488c7165d3e84a5bb0793b28244aea9a2fad8
Jeff Bai <j.bai@wustl.edu>
2020-12-24 08:22:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Steven", age: "30", attitude: "confused"}
-      ]
+        ]
    },
    :capulet => {
       :patriarch => {
