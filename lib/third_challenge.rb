98288da583570ec089e4ba413e885873b9991c4c
Jeff Bai <j.bai@wustl.edu>
2020-12-24 08:22:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,8 +15,8 @@ def third_challenge
           status: "alive"
         },
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Steven", age: "30", attitude: "confused"}
+          {name: "Benvolio", age: "17", attitude: "worried"},
+          {name: "Steven", age: "30", attitude: "confused"}
         ]
    },
    :capulet => {
